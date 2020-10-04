4bedcc8e5d56421cf0c843b1e4dddc953b24b03c
Chris Southwick <csouthwick@live.com>
2020-10-04 13:27:31 -0400

    add questions

--- index.js
+++ index.js
@@ -1,6 +1,58 @@
 // array of questions for user
 const questions = [
-
+  {
+    type: 'input',
+    name: 'username',
+    message: 'What is your GitHub username?'
+  },
+  {
+    type: 'input',
+    name: 'email',
+    message: 'What is your email address?'
+  },
+  {
+    type: 'input',
+    name: 'projectName',
+    message: 'What is your project\'s name?'
+  },
+  {
+    type: 'input',
+    name: 'description',
+    message: 'Please write a short description of your project'
+  },
+  {
+    type: 'list',
+    name: 'license',
+    message: 'What kind of license should your project have?',
+    choices: [
+      'Apache',
+      'BSD',
+      'GPL',
+      'LGPL',
+      'MIT',
+      'Mozilla'
+    ]
+  },
+  {
+    type: 'input',
+    name: 'installCmd',
+    message: 'What command should be run to install dependencies?'
+  },
+  {
+    type: 'input',
+    name: 'testCmd',
+    message: 'What command should be run to run tests?'
+  },
+  {
+    type: 'input',
+    name: 'usage',
+    message: 'What does the user need to know about using the repo?'
+  },
+  {
+    type: 'input',
+    name: 'contributing',
+    message: 'What does the user need to know about contributing to the repo?'
+  }
 ];
 
 // function to write README file
