592a2fec0c0ebd1e7329cef1f699f7ab8cf40702
Chris Southwick <csouthwick@live.com>
2020-10-04 15:53:14 -0400

    added readme markdown template

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,7 +1,57 @@
 // function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
+![GitHub License](https://img.shieldds.io/badge/License-${data.license}-blue.svg)
 
+## Description
+
+${data.description}
+
+## Table of Contents
+
+* [Installation](#installation)
+
+* [Usage](#usage)
+
+* [License](#license)
+
+* [Contributing](#contributing)
+
+* [Tests](#tests)
+
+* [Questions](#questions)
+
+## Installation
+
+To install necessary dependencies, run the following command:
+
+\`\`\`
+${data.installCmd}
+\`\`\`
+
+## Usage
+
+${data.usage}
+
+## License
+
+This project is licensed under the ${data.license} license.
+
+## Contributing
+
+${data.contributing}
+
+## Tests
+
+To run tests, run the following command:
+
+\`\`\`
+${data.testCmd}
+\`\`\`
+
+## Questions
+
+If you have any questions about the repo, open an issue or contact me directly at ${data.email}. You can find more of my work at [${data.username}](https://github.com/${data.username}/).
 `;
 }
 
