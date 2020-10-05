4da66d249e63568bdf396490bd4f687d8ec8d5d7
Chris Southwick <csouthwick@live.com>
2020-10-04 22:43:19 -0400

    added validation checks

--- index.js
+++ index.js
@@ -7,22 +7,54 @@ const questions = [
   {
     type: 'input',
     name: 'username',
-    message: 'What is your GitHub username?'
+    message: 'What is your GitHub username?',
+    validate: usernameInput => {
+      if (usernameInput) {
+        return true;
+      } else {
+        console.log('Please enter your GitHub username');
+        return false;
+      }
+    }
   },
   {
     type: 'input',
     name: 'email',
-    message: 'What is your email address?'
+    message: 'What is your email address?',
+    validate: emailInput => {
+      if (emailInput) {
+        return true;
+      } else {
+        console.log('Please enter your email address');
+        return false;
+      }
+    }
   },
   {
     type: 'input',
     name: 'title',
-    message: 'What is your project\'s name?'
+    message: 'What is your project\'s name?',
+    validate: titleInput => {
+      if (titleInput) {
+        return true;
+      } else {
+        console.log('Please enter your project\'s name');
+        return false;
+      }
+    }
   },
   {
     type: 'input',
     name: 'description',
-    message: 'Please write a short description of your project'
+    message: 'Please write a short description of your project',
+    validate: descriptionInput => {
+      if (descriptionInput) {
+        return true;
+      } else {
+        console.log('Please write a short description of your project');
+        return false;
+      }
+    }
   },
   {
     type: 'list',
@@ -40,22 +72,54 @@ const questions = [
   {
     type: 'input',
     name: 'installCmd',
-    message: 'What command should be run to install dependencies?'
+    message: 'What command should be run to install dependencies?',
+    validate: installCmdInput => {
+      if (installCmdInput) {
+        return true;
+      } else {
+        console.log('Please enter the command that should be run to install dependencies');
+        return false;
+      }
+    }
   },
   {
     type: 'input',
     name: 'testCmd',
-    message: 'What command should be run to run tests?'
+    message: 'What command should be run to run tests?',
+    validate: testCmdInput => {
+      if (testCmdInput) {
+        return true;
+      } else {
+        console.log('Please enter the command that should be run to run tests');
+        return false;
+      }
+    }
   },
   {
     type: 'input',
     name: 'usage',
-    message: 'What does the user need to know about using the repo?'
+    message: 'What does the user need to know about using the repo?',
+    validate: usageInput => {
+      if (usageInput) {
+        return true;
+      } else {
+        console.log('Please enter what the user needs to know about using the repo');
+        return false;
+      }
+    }
   },
   {
     type: 'input',
     name: 'contributing',
-    message: 'What does the user need to know about contributing to the repo?'
+    message: 'What does the user need to know about contributing to the repo?',
+    validate: contributingInput => {
+      if (contributingInput) {
+        return true;
+      } else {
+        console.log('Please enter what the user needs to know about contributing to the repo');
+        return false;
+      }
+    }
   }
 ];
 
